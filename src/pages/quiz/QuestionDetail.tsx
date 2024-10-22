f98b81fa3e1d0c0d7839ba0a37507ae677a6ea07
belltalion <xogus4026@gmail.com>
2024-10-22 19:11:25 +0900

    fix react hook form

--- src/pages/quiz/QuestionDetail.tsx
+++ src/pages/quiz/QuestionDetail.tsx
@@ -6,15 +6,18 @@ import { ChoiceItem } from "@/pages/quiz/Choice/ChoiceItem.tsx";
 import { Skeleton, Spin } from "antd";
 import { ChoiceAddButton } from "@/pages/quiz/ChoiceAddButton.tsx";
 import { QuestionTitle } from "@/pages/quiz/QuestionTitle.tsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Choice } from "@/interface/Question.ts";
 import { QuestionSubjectInput } from "@/pages/quiz/QuestionSubjectInput.tsx";
+import Error from "@/components/Error.tsx";
 
 export function QuestionDetail() {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
 
-  const { data, refetch } = useQuery({
+  const [type, setType] = useState();
+
+  const { data, refetch, isPending, isError } = useQuery({
     queryKey: ["quizDetail"],
     queryFn: () =>
       axios
@@ -23,83 +26,96 @@ export function QuestionDetail() {
             Authorization: localStorage.getItem("Authorization"),
           },
         })
-        .then((value) => value.data.data),
+        .then((value) => {
+          setType(value.data.data.type);
+          return value.data.data;
+        }),
   });
 
   useEffect(() => {
     refetch();
   }, [id, refetch]);
 
+  if (isPending) return <Spin />;
+
+  if (isError) return <Error />;
+
   return (
-    <Box
-      bgImage={"/images/play-bg.png"}
-      h={"100%"}
-      bgSize={"cover"}
-      bgPosition={"center"}
-      mx={"300px"}
-    >
-      <Center
-        mx={"auto"}
-        p={6}
-        w={"100%"}
+    data && (
+      <Box
+        bgImage={"/images/play-bg.png"}
         h={"100%"}
-        flexDir={"column"}
-        gap={10}
-        maxW={["700px", null, null, "800px", "900px"]}
+        bgSize={"cover"}
+        bgPosition={"center"}
+        mx={"300px"}
       >
-        <Flex
-          border={"white"}
-          bg={"white"}
+        <Center
+          mx={"auto"}
+          p={6}
           w={"100%"}
-          borderRadius={"24px"}
-          boxShadow={[0, null, "2px 2px 2px #646363"]}
+          h={"100%"}
+          flexDir={"column"}
+          gap={10}
+          maxW={["700px", null, null, "800px", "900px"]}
         >
+          <Flex
+            border={"white"}
+            bg={"white"}
+            w={"100%"}
+            borderRadius={"24px"}
+            boxShadow={[0, null, "2px 2px 2px #646363"]}
+          >
+            {data ? (
+              <QuestionTitle id={Number(id)} data={data} key={data.id} />
+            ) : (
+              <Spin />
+            )}
+          </Flex>
+          <Image src={"/images/img.png"} w={"80%"} />
           {data ? (
-            <QuestionTitle id={Number(id)} data={data} key={data.id} />
-          ) : (
-            <Spin />
-          )}
-        </Flex>
-        <Image src={"/images/img.png"} w={"80%"} />
-        {data ? (
-          data.type === 0 ? (
-            <Grid
-              w={"100%"}
-              gap={5}
-              gridTemplateColumns={["repeat(1, 1fr)", null, "repeat(2, 1fr)"]}
-            >
-              {data.choices.map((v: Choice, index: number) => (
-                <ChoiceItem
-                  {...v}
-                  index={index}
-                  key={v.id}
-                  unCorrectAble={
-                    data.choices.filter((v: Choice) => v.isCorrect).length === 1
-                  }
-                  deleteAble={data.choices.length !== 1}
+            [
+              <Grid
+                w={"100%"}
+                gap={5}
+                gridTemplateColumns={["repeat(1, 1fr)", null, "repeat(2, 1fr)"]}
+              >
+                {data.choices.map((v: Choice, index: number) => (
+                  <ChoiceItem
+                    {...v}
+                    index={index}
+                    key={v.id}
+                    unCorrectAble={
+                      data.choices.filter((v: Choice) => v.isCorrect).length ===
+                      1
+                    }
+                    deleteAble={data.choices.length !== 1}
+                  />
+                ))}
+                <Center>
+                  {data && data.choices.length < 8 && (
+                    <ChoiceAddButton questionId={Number(id)} />
+                  )}
+                </Center>
+              </Grid>,
+              <Flex
+                border={"white"}
+                bg={"white"}
+                w={"100%"}
+                borderRadius={"24px"}
+                boxShadow={[0, null, "2px 2px 2px #646363"]}
+              >
+                <QuestionSubjectInput
+                  id={Number(id)}
+                  data={data}
+                  key={data.id}
                 />
-              ))}
-              <Center>
-                {data && data.choices.length < 8 && (
-                  <ChoiceAddButton questionId={Number(id)} />
-                )}
-              </Center>
-            </Grid>
+              </Flex>,
+            ][type]
           ) : (
-            <Flex
-              border={"white"}
-              bg={"white"}
-              w={"100%"}
-              borderRadius={"24px"}
-              boxShadow={[0, null, "2px 2px 2px #646363"]}
-            >
-              <QuestionSubjectInput id={Number(id)} data={data} key={data.id} />
-            </Flex>
-          )
-        ) : (
-          <Skeleton />
-        )}
-      </Center>
-    </Box>
+            <Skeleton />
+          )}
+        </Center>
+      </Box>
+    )
   );
 }
--- src/pages/quiz/QuestionSetPanel.tsx
+++ src/pages/quiz/QuestionSetPanel.tsx
@@ -6,15 +6,14 @@ import { useSearchParams } from "react-router-dom";
 import Button from "@/components/Button.tsx";
 import { QuestionTypeSelect } from "@/pages/quiz/QuestionTypeSelect.tsx";
 import { Spin } from "antd";
+import Error from "@/components/Error.tsx";
 
 export function QuestionSetPanel() {
   const [searchParams] = useSearchParams();
+
   const id = searchParams.get("id");
-
-  const client = useQueryClient();
-
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["quizDetail"],
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["quizDetail", id],
     queryFn: async () =>
       await axios
         .get(import.meta.env.VITE_API_URL + "/api/v1/question/" + id, {
@@ -22,9 +21,20 @@ export function QuestionSetPanel() {
             Authorization: localStorage.getItem("Authorization"),
           },
         })
-        .then((value) => value.data),
+        .then((value) => value.data.data),
   });
 
+  if (isPending) return <Spin />;
+  if (isError) return <Error />;
+  return <A data={data} key={id} />;
+}
+function A({ data }) {
+  const client = useQueryClient();
+
+  const { handleSubmit, register } = useForm();
+  const [searchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
   const { mutate } = useMutation({
     mutationFn: async (value: any) => {
       return await axios.put(
@@ -45,128 +55,119 @@ export function QuestionSetPanel() {
     },
   });
 
-  const { handleSubmit, register } = useForm({
-    defaultValues: { ...data },
-  });
-
-  if (isLoading || isError) return null;
-  console.log(data);
-
   return (
     <form onSubmit={handleSubmit((data) => mutate(data))}>
-      {data ? (
-        <Box
-          w={"300px"}
-          bg={"whitesmoke"}
-          h={"100%"}
-          p={5}
-          pos={"absolute"}
-          right={0}
-          top={0}
-          overflowY={"scroll"}
-        >
-          <Flex gap={4} flexDir={"column"}>
-            <Flex flexDir={"column"} gap={2}>
-              <Text fontSize={"2xl"} color={"mainText"}>
-                퀴즈 유형
-              </Text>
-              <QuestionTypeSelect id={Number(id)} data={data} />
-            </Flex>
-            <Flex flexDir={"column"} gap={2}>
-              <Text fontSize={"2xl"} color={"mainText"}>
-                제한 시간
-              </Text>
-              <Input
-                key={data.id}
-                type={"number"}
-                {...register("options.time", {
-                  required: "제한 시간을 입력해주세요",
-                })}
-                defaultValue={data.options.time}
-                fontSize={"2xl"}
-                color={"mainText"}
-                w={"100%"}
-                textAlign={"end"}
-              />
-            </Flex>
-            <Flex flexDir={"column"} gap={2}>
-              <Text fontSize={"2xl"} color={"mainText"}>
-                배점
-              </Text>
-              <Input
-                key={data.id}
-                type={"number"}
-                {...register("options.score", {
-                  required: "점수를 입력해주세요",
-                })}
-                defaultValue={data.options.score}
-                fontSize={"2xl"}
-                color={"mainText"}
-                w={"100%"}
-                textAlign={"end"}
-              />
-            </Flex>
-            <Flex gap={2} justify={"space-between"}>
-              <Text fontSize={"2xl"} color={"mainText"}>
-                힌트 사용
-              </Text>
-              <Checkbox
-                key={data.id}
-                color={"primary"}
-                size={"lg"}
-                defaultValue={data.options.useHint}
-                {...register("options.useHint")}
-              />
-            </Flex>
-            <Flex flexDir={"column"} gap={2}>
-              <Text fontSize={"2xl"} color={"mainText"}>
-                힌트 제공 시간
-              </Text>
-              <Input
-                key={data.id}
-                type={"number"}
-                {...register("options.hintTime")}
-                defaultValue={data.options.hintTime}
-                fontSize={"2xl"}
-                color={"mainText"}
-                w={"100%"}
-                textAlign={"end"}
-              />
-            </Flex>
-            <Flex flexDir={"column"} gap={2}>
-              <Text fontSize={"2xl"} color={"mainText"}>
-                힌트 내용
-              </Text>
-              <Textarea
-                key={data.id}
-                defaultValue={data.options.hintContent}
-                {...register("options.hintContent")}
-                fontSize={"2xl"}
-                w={"100%"}
-                color={"mainText"}
-              />
-            </Flex>
-            <Flex flexDir={"column"} gap={2}>
-              <Text fontSize={"2xl"} color={"mainText"}>
-                해설
-              </Text>
-              <Textarea
-                key={data.id}
-                defaultValue={data.options.commentary}
-                {...register("options.commentary")}
-                fontSize={"2xl"}
-                color={"mainText"}
-                w={"100%"}
-              />
-            </Flex>
-            <Button type={"submit"} bg={"primary"}>
-              <Text color={"white"}>저장</Text>
-            </Button>
+      <Box
+        w={"300px"}
+        bg={"whitesmoke"}
+        h={"100%"}
+        p={5}
+        pos={"absolute"}
+        right={0}
+        top={0}
+        overflowY={"scroll"}
+      >
+        <Flex gap={4} flexDir={"column"}>
+          <Flex flexDir={"column"} gap={2}>
+            <Text fontSize={"2xl"} color={"mainText"}>
+              퀴즈 유형
+            </Text>
+            <QuestionTypeSelect id={Number(id)} data={data} />
           </Flex>
-        </Box>
-      ) : (
-        <Spin />
-      )}
+          <Flex flexDir={"column"} gap={2}>
+            <Text fontSize={"2xl"} color={"mainText"}>
+              제한 시간
+            </Text>
+            <Input
+              key={data.id}
+              type={"number"}
+              {...register("options.time", {
+                required: "제한 시간을 입력해주세요",
+                value: data?.options.time ?? "",
+              })}
+              defaultValue={data?.options.time ?? ""}
+              value={data?.options.time ?? ""}
+              fontSize={"2xl"}
+              color={"mainText"}
+              w={"100%"}
+              textAlign={"end"}
+            />
+          </Flex>
+          <Flex flexDir={"column"} gap={2}>
+            <Text fontSize={"2xl"} color={"mainText"}>
+              배점
+            </Text>
+            <Input
+              key={data.id}
+              type={"number"}
+              {...register("options.score", {
+                required: "점수를 입력해주세요",
+              })}
+              defaultValue={data.options.score}
+              fontSize={"2xl"}
+              color={"mainText"}
+              w={"100%"}
+              textAlign={"end"}
+            />
+          </Flex>
+          <Flex gap={2} justify={"space-between"}>
+            <Text fontSize={"2xl"} color={"mainText"}>
+              힌트 사용
+            </Text>
+            <Checkbox
+              key={data.id}
+              color={"primary"}
+              size={"lg"}
+              defaultValue={data.options.useHint}
+              {...register("options.useHint")}
+            />
+          </Flex>
+          <Flex flexDir={"column"} gap={2}>
+            <Text fontSize={"2xl"} color={"mainText"}>
+              힌트 제공 시간
+            </Text>
+            <Input
+              key={data.id}
+              type={"number"}
+              {...register("options.hintTime")}
+              defaultValue={data.options.hintTime}
+              fontSize={"2xl"}
+              color={"mainText"}
+              w={"100%"}
+              textAlign={"end"}
+            />
+          </Flex>
+          <Flex flexDir={"column"} gap={2}>
+            <Text fontSize={"2xl"} color={"mainText"}>
+              힌트 내용
+            </Text>
+            <Textarea
+              key={data.id}
+              defaultValue={data.options.hintContent}
+              {...register("options.hintContent")}
+              fontSize={"2xl"}
+              w={"100%"}
+              color={"mainText"}
+            />
+          </Flex>
+          <Flex flexDir={"column"} gap={2}>
+            <Text fontSize={"2xl"} color={"mainText"}>
+              해설
+            </Text>
+            <Textarea
+              key={data.id}
+              defaultValue={data.options.commentary}
+              {...register("options.commentary")}
+              fontSize={"2xl"}
+              color={"mainText"}
+              w={"100%"}
+            />
+          </Flex>
+          <Button type={"submit"} bg={"primary"}>
+            <Text color={"white"}>저장</Text>
+          </Button>
+        </Flex>
+      </Box>
     </form>
   );
 }
